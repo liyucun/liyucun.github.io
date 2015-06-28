465db11aaa19a398c5428f060a8db05ee016c6d2
yucun li <liyucun2012@gmail.com>
2015-06-28 14:45:03 -0400

    work experience done

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ var data = [
   { 'title' : '' , 'subtitle' : '#corso' , 'img' :  'image/corso.jpg' },
   { 'title' : '' , 'subtitle' : '#church' , 'img' :  'image/church.jpg' },
   { 'title' : '' , 'subtitle' : '#newtown' , 'img' :  'image/newtown.jpg' },
-  { 'title' : '' , 'subtitle' : '#yelp' , 'img' :  'image/yelp.jpg' },
+  { 'title' : '' , 'subtitle' : '#national patriots’ day' , 'img' :  'image/patriots.jpg' },
   { 'title' : '' , 'subtitle' : '#selfie' , 'img' :  'image/yucun.jpg' },
   { 'title' : '' , 'subtitle' : '#centiped' , 'img' :  'image/centiped.jpg' },
   { 'title' : '' , 'subtitle' : '#mont royal' , 'img' :  'image/mont_royal.jpg' },
   { 'title' : '' , 'subtitle' : '#french music festival' , 'img' :  'image/frenchfesti.jpg' },
+  { 'title' : '' , 'subtitle' : '#french music festival' , 'img' :  'image/catherine.jpg' },
 ];
 
 (function ( $ ) {
